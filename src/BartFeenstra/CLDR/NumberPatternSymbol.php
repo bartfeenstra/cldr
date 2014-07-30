5d178551b1ad7e0c08293c82d0605e49104da871
Bojan Zivanovic <bojanz@gmail.com>
2014-07-31 00:09:26 +0200

    Initial coding standards reformat uvia PHPStorm.

--- src/BartFeenstra/CLDR/NumberPatternSymbol.php
+++ src/BartFeenstra/CLDR/NumberPatternSymbol.php
@@ -10,49 +10,51 @@ namespace BartFeenstra\CLDR;
 /**
  * Describes a character from a number pattern .
  */
-class NumberPatternSymbol {
+class NumberPatternSymbol
+{
 
-  /**
-   * Whether the symbol is escaped or not.
-   *
-   * @var boolean
-   */
-  public $escaped = FALSE;
+    /**
+     * Whether the symbol is escaped or not.
+     *
+     * @var boolean
+     */
+    public $escaped = FALSE;
 
-  /**
-   * Whether this symbol escapes another or not.
-   *
-   * @var boolean
-   */
-  public $escapes_other_symbol = FALSE;
+    /**
+     * Whether this symbol escapes another or not.
+     *
+     * @var boolean
+     */
+    public $escapes_other_symbol = FALSE;
 
-  /**
-   * The symbol's position in the original pattern.
-   *
-   * @var integer
-   */
-  public $position = NULL;
+    /**
+     * The symbol's position in the original pattern.
+     *
+     * @var integer
+     */
+    public $position = NULL;
 
-  /**
-   * The symbol's replacement value.
-   *
-   * @var string
-   */
-  public $replacement = NULL;
+    /**
+     * The symbol's replacement value.
+     *
+     * @var string
+     */
+    public $replacement = NULL;
 
-  /**
-   * The symbol.
-   *
-   * @var string
-   */
-  public $symbol = NULL;
+    /**
+     * The symbol.
+     *
+     * @var string
+     */
+    public $symbol = NULL;
 
-  /**
-   * Implements __construct().
-   */
-  function __construct($symbol, $position = NULL, $escaped = FALSE) {
-    $this->symbol = $symbol;
-    $this->position = $position;
-    $this->escaped = $escaped;
-  }
+    /**
+     * Implements __construct().
+     */
+    function __construct($symbol, $position = NULL, $escaped = FALSE)
+    {
+        $this->symbol = $symbol;
+        $this->position = $position;
+        $this->escaped = $escaped;
+    }
 }
--- src/BartFeenstra/Tests/CLDR/PercentageFormatterTest.php
+++ src/BartFeenstra/Tests/CLDR/PercentageFormatterTest.php
@@ -14,16 +14,18 @@ require_once __DIR__ . '/../../../../vendor/autoload.php';
 /**
  * Tests \BartFeenstra\CLDR\PercentageFormatter
  */
-class PercentageFormatterTest extends \PHPUnit_Framework_TestCase {
+class PercentageFormatterTest extends \PHPUnit_Framework_TestCase
+{
 
-  /**
-   * Test amount formatting.
-   */
-  function testFormat() {
-    $formatter = new PercentageFormatter('0.00%‰');
-    $number = 123456.789;
-    $result_expected = '123456.789%‰';
-    $result = $formatter->format($number);
-    $this->assertSame($result, $result_expected, 'BartFeenstra\CLDR\PercentageFormatter::format() formats amount ' . $number . ' as ' . $result_expected . ' using pattern ' . $formatter->pattern . ' (result was ' . $result . ').');
-  }
+    /**
+     * Test amount formatting.
+     */
+    function testFormat()
+    {
+        $formatter = new PercentageFormatter('0.00%‰');
+        $number = 123456.789;
+        $result_expected = '123456.789%‰';
+        $result = $formatter->format($number);
+        $this->assertSame($result, $result_expected, 'BartFeenstra\CLDR\PercentageFormatter::format() formats amount ' . $number . ' as ' . $result_expected . ' using pattern ' . $formatter->pattern . ' (result was ' . $result . ').');
+    }
 }
